6b2d7426c479a2316fab3c8952dc1fdd415eb429
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2023-11-03 17:47:27 +0000

    fix ref warning, break variants

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,69 +1,33 @@
 import { Pressable } from "react-native";
-import { Text, Box } from "../../providers/theme";
+import { forwardRef, type ElementRef } from "react";
+import { Text, Box, Theme } from "../../providers/theme";
+import { VariantProps } from "@shopify/restyle";
 
-export enum ButtonVariant {
-  Primary = "primary",
-  Secondary = "secondary",
-  Ghost = "ghost",
-}
 interface ButtonProps {
   title: string;
   onPress?: () => void;
   disabled?: boolean;
-  variant?: ButtonVariant;
+  variant?: VariantProps<Theme, "buttonVariants">;
 }
 
-interface VariantDefValue {
-  backgroundColor: string;
-  color: string;
-}
-
-const variantDef = new Map<ButtonVariant, VariantDefValue>([
-  [
-    ButtonVariant.Primary,
-    {
-      backgroundColor: "primary",
-      color: "white",
-    },
-  ],
-  [
-    ButtonVariant.Secondary,
-    {
-      backgroundColor: "secondary",
-      color: "white",
-    },
-  ],
-  [
-    ButtonVariant.Ghost,
-    {
-      backgroundColor: "transparent",
-      color: "primary",
-    },
-  ],
-]);
-
-export function Button({
-  title,
-  onPress,
-  disabled,
-  variant = ButtonVariant.Primary,
-}: ButtonProps) {
-  const v = variantDef.get(variant);
-  if (!v) return false;
-  // console.log(variantDef.get(variant));
-  return (
-    <Pressable onPress={onPress} disabled={disabled}>
-      <Box
-        px="l"
-        py="m"
-        backgroundColor={v.backgroundColor}
-        borderRadius="m"
-        alignItems="center"
-      >
-        <Text color={v.color} variant="label">
-          {title}
-        </Text>
-      </Box>
-    </Pressable>
-  );
-}
+export const Button = forwardRef<ElementRef<typeof Pressable>, ButtonProps>(
+  function Button({ title, onPress, disabled, variant }, ref) {
+    const backgroundColor = "primary";
+    const color = "white";
+    return (
+      <Pressable onPress={onPress} disabled={disabled} ref={ref}>
+        <Box
+          px="l"
+          py="m"
+          backgroundColor={backgroundColor}
+          borderRadius="m"
+          alignItems="center"
+        >
+          <Text color={color} variant="label">
+            {title}
+          </Text>
+        </Box>
+      </Pressable>
+    );
+  },
+);
